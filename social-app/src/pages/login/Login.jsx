2d16199760c46adbacb3c1003c3db627eb0e4e77
Maverick Shreeve <maverickshreeve@gmail.com>
2023-05-10 22:03:06 -0700

    made startup page

--- social-app/src/pages/login/Login.jsx
+++ social-app/src/pages/login/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>hello world</h1>
+          <p>
+            gfiksdjgisghisghishgiushgiursdhgisuhgisughisughiursfhgisuhgisuhgisuhg
+            uhsgbujhwsgbgujshbggsujhgs
+          </p>
+          <span>no account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right"></div>
+        <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;

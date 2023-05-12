3cf602f25a9986c985c51f900c5ce19ab74f99f5
Maverick Shreeve <maverickshreeve@gmail.com>
2023-05-12 16:05:49 -0700

    making login and register pages fuller

--- social-app/src/pages/login/Login.jsx
+++ social-app/src/pages/login/Login.jsx
@@ -1,25 +1,36 @@
-import "./register.scss";
 
-const Register = () => {
+
+import { Link } from "react-router-dom";
+import "./login.scss";
+
+const Login = () => {
+  
+
+
   return (
-    <div className="register">
+    <div className="login">
       <div className="card">
         <div className="left">
-          <h1>Lama Social.</h1>
-          <p>gjrfdkfbgjhdrbgjdhbgjdrhbgjdrhgbjdrhgburdhgbudrhgburdb</p>
-          <span>Do you have an account?</span>
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+         
+          
+            <button>Register</button>
+            </Link>
+          
         </div>
         <div className="right">
-          <h1>Register</h1>
+          <h1>Login</h1>
           <form>
             <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
-            <button>Register</button>
+            <button>Login</button>
           </form>
         </div>
       </div>
@@ -27,4 +38,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
